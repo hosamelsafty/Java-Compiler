abdd63a77b2716c489060e6de8021f4849b7f779
Meena Alfons <meena.kerolos@gmail.com>
2018-03-22 00:40:53 +0200

    Starting better NFA

--- lib/NFATransitionTable.cpp
+++ lib/NFATransitionTable.cpp
@@ -1,8 +1,16 @@
 #include "NFATransitionTable.h"
+#include <algorithm>
+#include <iterator>
+#include <assert.h>
 
 struct NFATransitionTable::impl
 {
-	std::map < std::set<State>, std::map<char, std::set<State> > > table;
+	std::vector< Transition > transitions;
+	std::map< State, std::set<int> > fromIndex;
+	std::map< State, std::set<int> > toIndex;
+
+	std::set<State> startingStates;
+	std::set<State> endingStates;
 };
 
 NFATransitionTable::NFATransitionTable()
@@ -14,7 +22,7 @@ NFATransitionTable::NFATransitionTable()
 NFATransitionTable::NFATransitionTable(const NFATransitionTable & t)
 	: m_d(std::make_unique<impl>())
 {
-	m_d->table = t.m_d->table;
+	// TODO
 }
 
 NFATransitionTable::~NFATransitionTable()
@@ -22,23 +30,147 @@ NFATransitionTable::~NFATransitionTable()
 
 }
 
-void NFATransitionTable::add(const std::set<State> &currentStates, char input, const std::set<State> &nextStates)
+const NFATransitionTable & NFATransitionTable::operator=(const NFATransitionTable &nfa)
 {
-	m_d->table[currentStates][input] = nextStates;
+	// TODO
 }
 
-std::set<State> NFATransitionTable::nextStates(const std::set<State> &currentStates, char input) const
+void NFATransitionTable::setTransition(const State &fromState, NFATransitionTable::Input input, const State &toState)
 {
-	return m_d->table[currentStates][input];
+	std::set<int> fromIndicies = m_d->fromIndex[fromState];
+	std::set<int> fromIndicies = m_d->toIndex[toState];
+	std::set<int> possibleCollision;
+	set_intersection(fromIndicies.begin(), fromIndicies.end(), fromIndicies.begin(), fromIndicies.end(),
+		std::inserter(possibleCollision, possibleCollision.begin()));
+
+	for (int i : possibleCollision)
+	{
+		if (std::get<1>(m_d->transitions[i]) == input)
+		{
+			// No need to add the same transition again
+			return;
+		}
+	}
+
+	// That transition is not found. Add it.
+	m_d->transitions.push_back(Transition(fromState, input, toState));
+	int index = m_d->transitions.size() - 1;
+	m_d->fromIndex[fromState].insert(index);
+	m_d->toIndex[toState].insert(index);
 }
 
-
-std::map<char, std::set<State> > NFATransitionTable::getMapping(const std::set<State> &currentStates) const
+std::vector<NFATransitionTable::Transition> NFATransitionTable::getAllTransitions() const
 {
-    return m_d->table[currentStates];
-};
+	return m_d->transitions;
+}
 
-std::set<State> NFATransitionTable::getStartingSet() const
+std::set<State> NFATransitionTable::epsClosure(const State &state) const
 {
-    return  startingSet;
+
+}
+std::set<State> NFATransitionTable::epsClosure(const std::set<State> &states) const
+{
+
+}
+std::set<State> NFATransitionTable::move(const std::set<State> &states, char input) const
+{
+
+}
+
+void NFATransitionTable::setStartingStates(const std::set<State> &states)
+{
+	m_d->startingStates = states;
+}
+
+void NFATransitionTable::addStartingState(const State &state)
+{
+	m_d->startingStates.insert(state);
+}
+
+void NFATransitionTable::removeStartingState(const State &state)
+{
+	m_d->startingStates.erase(state);
+}
+
+void NFATransitionTable::setAcceptingStates(const std::set<State> &states)
+{
+	m_d->endingStates = states;
+}
+
+void NFATransitionTable::addAcceptingStates(const State &state)
+{
+	m_d->endingStates.insert(state);
+}
+
+void NFATransitionTable::removeAcceptingStates(const State &state)
+{
+	m_d->endingStates.erase(state);
+}
+
+std::set<State> NFATransitionTable::getStartingStates() const
+{
+	return m_d->startingStates;
+}
+
+std::set<State> NFATransitionTable::getAcceptingStates() const
+{
+	return m_d->endingStates;
+}
+
+NFATransitionTable NFATransitionTable::opUnion(const NFATransitionTable &rhs) const
+{
+	NFATransitionTable result;
+
+	// Depending on the uniqueness of State we will add the states from *this and rhs to result.
+
+	// TODO Add all transition of *this to result
+	
+	// TODO Add all transition of rhs to result
+
+	// TODO Create resultStartingState
+
+	// TODO Add to result: transitions from resultStartingState to all startingStates of *this 
+
+	// TODO Add to result: transitions from resultStartingState to all startingStates of rhs
+
+	// TODO Create resultEndingState
+
+	// TODO Add to result: transitions from all endingStates of *this to resultEndingState.
+
+	// TODO Add to result: transitions from all endingStates of rhs to resultEndingState.
+
+	return result;
+}
+
+NFATransitionTable NFATransitionTable::opConcat(const NFATransitionTable &rhs) const
+{
+	// *this must have one endingState and rhs must have one starting state.
+	assert(this->getAcceptingStates().size() == 1);
+	assert(rhs.getAcceptingStates().size() == 1);
+
+	NFATransitionTable result;
+
+	// Depending on the uniqueness of State we will add the states from *this and rhs to result.
+
+	// TODO Add all transition of *this to result
+
+	// TODO Add all transition of rhs to result.
+	// Special case for transitions starting at the startingState of rhs.
+	// The from part of these transitions will be replaced by the endingState *this when being added to result.
+
+	// TODO Set startingState of result to be the starting state of *this.
+
+	// TODO Set endingState of result to be the ending state of rhs.
+
+	return result;
+}
+
+NFATransitionTable NFATransitionTable::opPlus() const
+{
+
+}
+
+NFATransitionTable NFATransitionTable::opStar() const
+{
+
 }
--- lib/NFATransitionTable.h
+++ lib/NFATransitionTable.h
@@ -7,21 +7,49 @@
 #include <memory>
 #include <vector>
 #include <map>
+#include <tuple>
 
 #include "State.h"
 
 class NFATransitionTable
 {
 public:
+	typedef char Input;
+
+	// TODO epsilon symbol need to be defined
+	static const char EPS;
+
+	typedef std::tuple<State, Input, State> Transition;
+
     NFATransitionTable();
-    NFATransitionTable(const NFATransitionTable &);
+    NFATransitionTable(const NFATransitionTable &nfa);
     ~NFATransitionTable();
 
-    void add(const std::set<State> &currentStates, char input, const std::set<State> &nextStates);
-    std::set<State> nextStates(const std::set<State> &currentStates, char input) const;
-	std::map<char, std::set<State> > getMapping(const std::set<State> &currentStates) const;
-	std::set<State> getStartingSet() const;
-	std::set<State> startingSet;
+	const NFATransitionTable &operator=(const NFATransitionTable &nfa);
+
+	void setTransition(const State &fromState, Input input, const State &toState);
+
+	std::vector<Transition> getAllTransitions() const;
+
+	std::set<State> epsClosure(const State &state) const;
+	std::set<State> epsClosure(const std::set<State> &states) const;
+	std::set<State> move(const std::set<State> &states, char input) const;
+
+	void setStartingStates(const std::set<State> &states);
+	void addStartingState(const State &state);
+	void removeStartingState(const State &state);
+
+	void setAcceptingStates(const std::set<State> &states);
+	void addAcceptingStates(const State &state);
+	void removeAcceptingStates(const State &state);
+
+	std::set<State> getStartingStates() const;
+	std::set<State> getAcceptingStates() const;
+
+	NFATransitionTable opUnion(const NFATransitionTable &rhs) const;
+	NFATransitionTable opConcat(const NFATransitionTable &rhs) const;
+	NFATransitionTable opPlus() const;
+	NFATransitionTable opStar() const;
 
 private:
 	struct impl;
